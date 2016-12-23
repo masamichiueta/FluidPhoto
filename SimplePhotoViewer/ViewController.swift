461c00cdbcdd60ff1926330a870c198eeb9aa8ff
micchyboy1023 <masamichi1023@gmail.com>
2016-12-23 22:49:57 +0900

    Initial Commit

---
+++ SimplePhotoViewer/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  SimplePhotoViewer
+//
+//  Created by UetaMasamichi on 2016/12/23.
+//  Copyright © 2016 Masmichi Ueta. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
